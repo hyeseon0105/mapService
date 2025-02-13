06a659d7e52172dba9ffdcf073d82a1e97a2c4c4
hyeseon0105 <hyesun0536@gmail.com>
2025-02-13 17:51:00 +0900

    #3-스크립트처리완료(jquery)

---
+++ src/main/resources/static/js/member.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+  $("form").on("submit",function(event){
+    if($("#name").val()==""){
+     event.preventDefault();
+      alert("이름입력")
+      $("#name").focus()
+      return
+    }
+    if($("#addr").val()==""){
+     event.preventDefault();
+      alert("주소입력")
+      $("#addr").focus()
+      return
+    }
+  })
+})
